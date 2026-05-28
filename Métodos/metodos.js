b533953da79863a664a63f49bc52e593c4fee47b
Arcane3308 <gustavo.alves.santos26@escola.pr.gov.br>
2026-05-28 09:09:10 -0300

    Add files via upload

---
+++ Métodos/metodos.js
@@ -0,0 +1,27 @@
+class Monstro{
+    constructor(nome,hp){
+        this.nome = nome;
+        this.hp = Number(hp);
+    }
+    receberDano(dano){
+        this.hp -= dano;
+        if(this.hp <= 0){
+            this.hp = 0;
+            return `${this.nome} foi <strong>eliminado</strong>`
+        }
+        return `${this.nome} recebeu um ataque que deu <strong>${dano}</strong> de dano!`
+    }
+}
+
+let monstroBatalha;
+function iniciarJogo(){
+
+    const nome = document.getElementById('nomeMonstro').value;
+    const hp = document.getElementById('hpMonstro').value;
+
+    monstroBatalha = new Monstro(nome,hp);
+
+    function atualizaTela(){
+        document.getElementById()
+    }
+}
