2f9dc37c81a6e0c782a2803b53902697e2e9415f
Arcane3308 <gustavo.alves.santos26@escola.pr.gov.br>
2026-06-24 11:53:53 -0300

    Add files via upload

---
+++ Curso_Java/aula6.java
@@ -0,0 +1,10 @@
+public class aula6{
+    
+    public static void main(String[] args){
+        int energia = 0;
+        
+        do{
+            System.out.println("Testando o dowhile");
+        } while(energia > 0);
+}
+}
